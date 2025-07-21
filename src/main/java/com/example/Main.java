a7f4d81ca4129416be2ce56b3e85781e64fd002c
shanjing <2067068392@qq.com>
2025-07-21 22:04:28 +0800

    letcode-1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,4 +5,24 @@ public class Main {
         System.out.println("first change code");
 
     }
+
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        if (nums[0] == slow) return slow;
+        slow = nums[0];
+        do{
+            slow = nums[slow];
+            fast = nums[fast];
+        }while(slow != fast);
+
+        return slow;
+        
+    }
 }
