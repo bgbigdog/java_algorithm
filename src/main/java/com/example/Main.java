c988e5252e15cbb718e05eb7af8c3b6ea97e186a
shanjing <2067068392@qq.com>
2025-07-23 20:48:59 +0800

    letcode-4

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -93,4 +93,45 @@ public class Main {
 
         return res[n-1];
     }
+
+    public int minCostClimbingStairs(int[] cost) {
+        if(cost.length == 1)return cost[0];
+
+        int dp[] = new int[cost.length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for(int i = 2; i < dp.length; i++){
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[dp.length - 1];
+    }
+
+    public int uniquePaths(int m, int n) {
+
+        if (m == 1) {
+            return 1;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int dp[][] = new int [m][n];
+        for(int i = 0; i < n; i++){
+            dp[0][i] = 1;
+        }
+
+        for(int i = 0; i < m; i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
 }
